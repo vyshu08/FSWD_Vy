84098e14542886ab24811776c92c39969161e8e7
vyshu08 <50903285+vyshu08@users.noreply.github.com>
2024-08-13 00:46:11 +0530

    Create login.validator.js

---
+++ Server/middleware/login.validator.js
@@ -0,0 +1,9 @@
+
+exports.loginValidator = (req,res,next)=>{
+    const {username,password} = req.body;
+    if(req.body&&username&&password){
+        next()
+    }else{
+        res.status(404).send({msg:"All input fields are required"})
+    }
+}
