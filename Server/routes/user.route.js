d583adfb54c955cf1425b090654de4dc88cd3d01
vyshu08 <50903285+vyshu08@users.noreply.github.com>
2024-08-13 00:42:06 +0530

    Create user.route.js

---
+++ Server/routes/user.route.js
@@ -0,0 +1,21 @@
+New! Keyboard shortcuts … Drive keyboard shortcuts have been updated to give you first-letters navigation
+const express = require("express");
+const { userSignUp, userLogin, getUserDetails } = require("../controller/user.controller");
+const { signupValidator } = require("../middleware/signup.validator");
+const { loginValidator } = require("../middleware/login.validator");
+const { authenticateUser } = require("../middleware/authenticateUser.js");
+
+
+const userRoute = express.Router();
+
+
+userRoute.post("/signup",signupValidator,userSignUp);
+
+userRoute.post("/login",loginValidator,userLogin);
+
+userRoute.get("/",authenticateUser,getUserDetails)  
+
+
+module.exports ={
+    userRoute
+}
